c67c8725abf8babd2ae3543598754e8400803ca6
captainameerica <ameersyed380@gmail.com>
2025-09-27 14:23:05 -0400

    test4

--- comfortquest/src/main.jsx
+++ comfortquest/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Welcome from './components/landing/Welcome.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Welcome />
   </StrictMode>,
 )
